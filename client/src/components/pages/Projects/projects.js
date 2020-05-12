0bd071b2fb8444dcadfa181a901500b78425967a
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-05-13 00:46:08 +1000

    delete image file

--- client/src/components/pages/Projects/projects.js
+++ client/src/components/pages/Projects/projects.js
@@ -12,7 +12,6 @@ function Projects() {
         {
           "id": 1,
           "name": "Movie App",
-          "image": "../../assets/images/digiPic01.jpg",
           "alt": "screenshot of Movie Review Website",
           "description": 
           "This is a movie review website where users can look up any movie and see reviews. This could help them decide to watch a movie or not. Secondly, users can register as a member and this allows them to also add their own reviews. They can also go into their profile page and delete any of their reviews that they are not happy with.",
@@ -22,7 +21,6 @@ function Projects() {
         {
         "id": 2,
         "name": "React Employee Directory",
-        "image": "../../assets/images/digiPic02.jpg",
         "alt": "screenshot of Employee Directory App",
         "description": 
         "This application is an employee directory that was created using React. It allows the user to filter their employees to easily find the details of specific people. It also allows the user to sort through the names in alphabetical and reverse.", 
@@ -32,7 +30,6 @@ function Projects() {
       {
         "id": 3,
         "name": "React Employee Directory",
-        "image": "../../assets/images/digiPic03.jpg",
         "alt": "screenshot of Employee Directory App",
         "description": 
         "This application is an employee directory that was created using React. It allows the user to filter their employees to easily find the details of specific people. It also allows the user to sort through the names in alphabetical and reverse.", 
