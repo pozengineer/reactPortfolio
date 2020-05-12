5afd004a65545d6bf68c8d07b052d48c5831c32d
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-05-13 02:07:47 +1000

    just trying to load projects page

--- client/src/components/pages/Projects/projects.js
+++ client/src/components/pages/Projects/projects.js
@@ -11,55 +11,9 @@ import "./style.css";
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function Projects() {
-    const projectsArray = [
-        {
-          "id": 1,
-          "name": "Movie App",
-          "image": digiPic01,
-          "alt": "screenshot of Movie Review Website",
-          "description": 
-          "This is a movie review website where users can look up any movie and see reviews. This could help them decide to watch a movie or not. Secondly, users can register as a member and this allows them to also add their own reviews. They can also go into their profile page and delete any of their reviews that they are not happy with.",
-          "url": "https://lit-peak-86961.herokuapp.com/",
-          "github": "https://github.com/Tonnette/Project2"
-        },
-        {
-        "id": 2,
-        "name": "React Employee Directory",
-        "image": digiPic02,
-        "alt": "screenshot of Employee Directory App",
-        "description": 
-        "This application is an employee directory that was created using React. It allows the user to filter their employees to easily find the details of specific people. It also allows the user to sort through the names in alphabetical and reverse.", 
-        "url": "https://tonnette-employee-react-app.herokuapp.com/",
-        "github": "https://github.com/Tonnette/React-Employee-Directory"
-      },
-      {
-        "id": 3,
-        "name": "React Employee Directory",
-        "image": digiPic03,
-        "alt": "screenshot of Employee Directory App",
-        "description": 
-        "This application is an employee directory that was created using React. It allows the user to filter their employees to easily find the details of specific people. It also allows the user to sort through the names in alphabetical and reverse.", 
-        "url": "https://tonnette-employee-react-app.herokuapp.com/",
-        "github": "https://github.com/Tonnette/React-Employee-Directory"
-      }
-    ]
-
     return (
-        <Container>
-            <Row>
-                <Col md={12} >
-                    <h1><b>Projects</b></h1>
-                    <hr />
-                </Col>
-            </Row>
-            <Row>
-                {projectsArray && projectsArray.map(element => {
-                    return (
-                        <ProjectCard key={element.id} project={element} />
-                    )
-                })}
-            </Row>
-        </Container>
+        <h1><b>Projects</b></h1>
+        <hr />
     );
 }
 
