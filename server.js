69ae770e030891bf28bdfc1c14cfb8d3cdf93434
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-05-13 02:34:10 +1000

    added cors to server.js

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
 // const routes = require("./routes");
+
 const app = express();
+// const User = require("./models/User.js");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 // Define middleware here
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.static("public"));
+
+app.get(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // Add routes, both API and view
 // app.use(routes);
 
