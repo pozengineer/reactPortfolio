cc608768a0a406541150c8214d6cbf58c12d4ce4
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-05-13 01:50:02 +1000

    imges won't load

--- client/src/components/pages/Projects/projects.js
+++ client/src/components/pages/Projects/projects.js
@@ -15,6 +15,7 @@ function Projects() {
         {
           "id": 1,
           "name": "Movie App",
+          "image": digiPic01,
           "alt": "screenshot of Movie Review Website",
           "description": 
           "This is a movie review website where users can look up any movie and see reviews. This could help them decide to watch a movie or not. Secondly, users can register as a member and this allows them to also add their own reviews. They can also go into their profile page and delete any of their reviews that they are not happy with.",
@@ -24,6 +25,7 @@ function Projects() {
         {
         "id": 2,
         "name": "React Employee Directory",
+        "image": digiPic02,
         "alt": "screenshot of Employee Directory App",
         "description": 
         "This application is an employee directory that was created using React. It allows the user to filter their employees to easily find the details of specific people. It also allows the user to sort through the names in alphabetical and reverse.", 
@@ -33,6 +35,7 @@ function Projects() {
       {
         "id": 3,
         "name": "React Employee Directory",
+        "image": digiPic03,
         "alt": "screenshot of Employee Directory App",
         "description": 
         "This application is an employee directory that was created using React. It allows the user to filter their employees to easily find the details of specific people. It also allows the user to sort through the names in alphabetical and reverse.", 
