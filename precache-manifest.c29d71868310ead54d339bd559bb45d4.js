ada726b8b560d44d112a924fc573e6938ca28c50
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-05-12 22:52:04 +1000

    Updates

---
+++ precache-manifest.c29d71868310ead54d339bd559bb45d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955b830e7214b40b8cf89b4445428347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afcd3eefc4225bbd7057",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c2e54674cd9634912777",
+    "url": "/static/css/main.aa520751.chunk.css"
+  },
+  {
+    "revision": "afcd3eefc4225bbd7057",
+    "url": "/static/js/2.085b0554.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.085b0554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e54674cd9634912777",
+    "url": "/static/js/main.8211e6ac.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "61974699f7f74b628e85cd998c0f6920",
+    "url": "/static/media/pozLogoHtml.61974699.jpg"
+  },
+  {
+    "revision": "b0fdd5f9d7573e4ffa5399f6117e862b",
+    "url": "/static/media/profilePic01.b0fdd5f9.jpg"
+  },
+  {
+    "revision": "46b39f5b648cd689dc43714ffd027a73",
+    "url": "/static/media/resumeMkIII.46b39f5b.pdf"
+  }
+]);
